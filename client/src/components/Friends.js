cb403d4e2241260ae585c639b44b731eb9c9d1aa
Joscandy Nunez <50030017+jcandyn@users.noreply.github.com>
2019-12-11 06:31:25 -0500

    added chat functionality

--- client/src/components/Friends.js
+++ client/src/components/Friends.js
@@ -9,85 +9,28 @@ class Friends extends React.Component {
   
    constructor(props) {
     super(props);
-    this.acceptRequest = this.acceptRequest.bind(this);
-    this.declineRequest = this.declineRequest.bind(this);
+  
     this.state = {
         FriendRequestsData: []
 };
+this.message = this.message.bind(this);
 }
 
-   acceptRequest = (friendId,userId,e) => {
-    e.preventDefault()
-       alert('accepting request')
-       alert(userId)
-       alert(friendId)
-       database.ref('users/' + userId + "/friendRequests/" + friendId).set({
-      "accepted": true
-      });
+componentDidMount = () => {
 
-
-      let tempArray = []
-      let childData;
-      var leadsRef = database.ref('users/' + userId + "/acceptedFriends");
-
-      leadsRef.on('value', snapshot => {
-          childData = snapshot.val();
-         console.log("tempArray",childData)
-        if (childData) {
-            tempArray = childData.friendId
-        }
-        
-      });
-        if (!tempArray.includes(friendId)){
-            tempArray.push(friendId)
-        }
-        
-    
-    
-    //   for now this method will make a node for friends accepted
-    database.ref('users/' + userId +"/acceptedFriends/").set({
-        "friendId": tempArray
-        });
-
-this.props.getFriends()
-   }
-
-   declineRequest (friendId,userId,e) {
-e.preventDefault()
-    alert('declining request')  
-    alert(userId)
-    alert(friendId)
-    database.ref('users/' + userId + "/friendRequests/" + friendId).update({
-        accepted: null
-        });
-   }
-
-    friendRequests = () => {
-  
-        let data = this.props.data
-        if(data === 0)
-            return null 
-         else {
-       
-            let childData;
-            let friendsRequests = this.state.FriendRequestsData
-            var leadsRef = database.ref('users/' + data);
-            leadsRef.on('value', snapshot => {
-                childData = snapshot.val();
-                friendsRequests.push(childData)
-     
-         })
- 
-         this.setState({
-            FriendRequestsData: friendsRequests
-        })
-    }
 }
+
+message = (friendId,userId,e) => {
+alert(friendId)
+alert(userId)
+
+}
+
 
 render() {
     return(
         <div>
-            {console.log(this.props.friends)}
+            {console.log("what is this?",this.props.friends)}
             {this.props.friends ?
           
             <div className='UserCard'>
@@ -99,8 +42,8 @@ render() {
             <p>{this.props.friends.occupation}</p>
             <p>{this.props.friends.bio}</p>
             <p>{this.props.friends.location}</p>
-            <p>{this.props.friends.bio}</p>
-            <button  onClick={e => {this.acceptRequest(this.state.FriendRequestsData[0].userId,this.props.thisUser,e)}}className="btn">Message</button>
+            {/* <button  onClick={e => {this.message(this.props.friends.userId,this.props.thisUser,e)}}className="btn">Message</button> */}
+              <a href={`/message/${this.props.friends.userId + "&" + this.props.thisUser}`} className="btn">Message</a>
           </div>
         </div>
         
